18bf63019460b782acca6fb3bdc02c79276296bb
Nicolas Rico <nricom@eafit.edu.co>
2025-11-04 10:27:15 -0500

    feat: Add CORS configuration to settings.py to specify allowed methods and headers for enhanced API security

--- backend/FikaFood/settings.py
+++ backend/FikaFood/settings.py
@@ -210,6 +210,25 @@ else:
     CORS_ALLOW_ALL_ORIGINS = DEBUG
 
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 # Security settings for production
 if not DEBUG:
